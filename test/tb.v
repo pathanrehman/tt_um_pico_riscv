de90ae553905cc9ff5e607ece84ce5549b74dd86
pathanrehman <52851945+pathanrehman@users.noreply.github.com>
2025-08-29 14:54:02 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -22,20 +22,19 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
+
 `ifdef GL_TEST
   wire VPWR = 1'b1;
   wire VGND = 1'b0;
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-
+  tt_um_pico_riscv user_project (
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
       .VPWR(VPWR),
       .VGND(VGND),
 `endif
-
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
@@ -46,4 +45,19 @@ module tb ();
       .rst_n  (rst_n)     // not reset
   );
 
+  // Additional debug signals for PicoRISC-V educational visibility
+  wire [15:0] current_instruction = user_project.instruction_reg;
+  wire [7:0] register_0 = user_project.registers[0];
+  wire [7:0] register_1 = user_project.registers[1];
+  wire [7:0] register_2 = user_project.registers[2];
+  wire [7:0] register_3 = user_project.registers[3];
+  wire [7:0] register_4 = user_project.registers[4];
+  wire [7:0] register_5 = user_project.registers[5];
+  wire [7:0] register_6 = user_project.registers[6];
+  wire [7:0] register_7 = user_project.registers[7];
+  wire [7:0] program_counter = user_project.pc;
+  wire instruction_valid = user_project.instruction_valid;
+  wire branch_taken = user_project.branch_taken;
+  wire [7:0] alu_result = user_project.alu_result;
+  
 endmodule
